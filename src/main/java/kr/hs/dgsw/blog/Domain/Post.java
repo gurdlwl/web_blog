f75284b950c0e85d7319cc8b4cef3a009ac35afc
JunHyuk Im <31408848+gurdlwl@users.noreply.github.com>
2019-04-22 15:58:53 +0900

    [UPDATE]

--- src/main/java/kr/hs/dgsw/blog/Domain/Post.java
+++ src/main/java/kr/hs/dgsw/blog/Domain/Post.java
@@ -40,4 +40,52 @@ public class Post {
         this.modified = p.getModified();
         this.postFilePath = p.getPostFilePath();
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public LocalDateTime getModified() {
+        return modified;
+    }
+
+    public void setModified(LocalDateTime modified) {
+        this.modified = modified;
+    }
+
+    public String getPostFilePath() {
+        return postFilePath;
+    }
+
+    public void setPostFilePath(String postFilePath) {
+        this.postFilePath = postFilePath;
+    }
 }
